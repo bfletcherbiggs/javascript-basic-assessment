30973b8f7c679513642aea54bb80ba3d0795c8ac
Benjamin Biggs <benjaminbiggs@MacBook-Pro.local>
2017-02-15 16:07:53 -0600

    God help me read better

--- assessment.js
+++ assessment.js
@@ -1,10 +1,11 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
-
+var hello = "goodbye"
 
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
+var iLoveCode = iLove + " code"
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -12,7 +13,16 @@ var iLove = 'I love';
 // bob has hair, that has style spikey, and color brown (object)
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
-
+var bob = {
+	height: '6ft',
+	age: 24,
+	hair:{
+		style: "spikey",
+		color: "brown"
+	},
+	presidentOfTheUnitedStates:false,
+	likes: ['apples','bananas','cherries']
+};
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
@@ -20,16 +30,25 @@ var myShirt = {
 	color: 'red'
 };
 
+myShirt.color = "pink"
+
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
 
+myOtherShirt['type'] = 'spandex'
+
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
+var zoo = {
+	monkeys: 8,
+	giraffes: 4,
+	elephants: 2
+}
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -41,49 +60,106 @@ var snake = {
 	runs: 'legless'
 };
 
-//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+for (var ele in snake){
+	if(ele.charAt(0) === "s"){snake[ele] = "s"}
+}
 
+//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+var rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+heroes.push('wonderwoman')
 // remove 'superman' and store him in a variable called firstHero
-
+var firstHero = heroes.shift();
 // add 'spongebob' to the start of the array
-
+heroes.unshift('spongebob')
 // remove 'flash' from the array and store him in a variable called secondHero
-
+var secondHero = heroes.splice(heroes.indexOf('flash'),1).join()
 // leave batman in the array but put a copy of him on a variable called thirdHero
-
+var thirdHero = heroes[heroes.indexOf('batman')]
 
 
 
 // #9 Write a function called addItem that takes in an array, an item, and adds the item to
 // the array.
-
+function addItem(users,item) {
+	users.push(item)
+	return users;
+}
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
 
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
 
+function removeItem(arr,str){
+	return arr.filter(ele => ele!==str)
+}
+
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
-
+function doubleTheFun(x){
+	if (isNaN(x)){
+		return x + x
+	}
+	else{
+		return x*=2;
+	}
+}
 
 
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
-
+function getValueOfProperty(obj,property){
+	return obj[property];
+}
 
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
+function makeChatMessage(str,signer){
+	return obj = {
+		message: str,
+		author: signer,
+		timestamp: new Date()
+	}
+}
 
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
-
+function coderTest(obj){
+	if (obj.name === 'Jeremy'){
+		obj.lovesCode = 10;
+	}
+	else if (obj.name === 'Brack'){
+		obj.lovesCode = 0;
+	}
+	else {
+		obj.lovesCode = 5;
+	}
+}
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
+function outside(t,h,c){
+	if (t>80) {
+		if (h<40 && c<20){
+			return "I'm literally in the desert"
+		}
+		else if (h>40){
+			return "I'm all sweat"
+		}
+		return "Hmm, probably not"
+	}
+	else if (t<40 && c>60) {
+		return "I have icecicles"
+	}
+	else if (h>50 || c>50){
+		return "Hmm, probably not"
+	}
+	else return "I love outside"
+}
+
+
 /*
     temperature over 80 and humidity over 40 - return "I'm all sweat"
     temperature under 40 and cloudiness over 60 - return "I have icecicles"
@@ -93,5 +169,12 @@ var heroes = ['superman', 'batman', 'flash'];
 */
 
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
-// example - If I call you with 'Give it' you should invoke holla with 'Give it back'
+// // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
 
+callerBack("Give it", function(holla) {
+	return holla;
+})
+
+function callerBack(cb,back){
+	cb(back + ' back')
+}
